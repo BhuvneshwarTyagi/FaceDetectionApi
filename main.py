ce639a084a9af27eb60ffa6e9f836c6c1a0ddfcf
arunjijhmaniya <153539176+arunjijhmaniya@users.noreply.github.com>
2023-12-11 22:41:39 +0530

    Update main.py
    Signed-off-by: arunjijhmaniya <153539176+arunjijhmaniya@users.noreply.github.com>

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ model = tf.keras.models.load_model('face_recognition_model.h5')
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 @app.post("/predict")
 async def predict(file: UploadFile = File('...')):
-    image=await file.read()
-    gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    contents = await file.read()
+    
+    # Convert raw bytes to image
+    nparr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
     faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
    # contents = await file.read()
         # Draw rectangles around the faces and save the images
@@ -36,8 +39,8 @@ async def predict(file: UploadFile = File('...')):
             #file_path = os.path.join(dataset, f'{count}face1.jpg')
            # print("File path is :",file_path)
         #cv2.imwrite(contents, face_roi)
-    contents=await face_roi.read()
-    image = Image.open(io.BytesIO(contents)).convert("RGB")
+    #contents=await face_roi.read()
+    #image = Image.open(io.BytesIO(contents)).convert("RGB")
     image = image.resize((64, 64))  # Resize as needed
     image_array = np.asarray(image)
     image_array = np.expand_dims(image_array, axis=0)  # Add batch dimension
