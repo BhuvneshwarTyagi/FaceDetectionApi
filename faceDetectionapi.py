4cb8f36257b3bb555946bb3dae6ff2db3b091453
Bhuvneshwar Tyagi <83375364+BhuvneshwarTyagi@users.noreply.github.com>
2023-12-12 16:33:54 +0530

    Add files via upload
    Signed-off-by: Bhuvneshwar Tyagi <83375364+BhuvneshwarTyagi@users.noreply.github.com>

---
+++ faceDetectionapi.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, UploadFile
+import cv2
+import numpy as np
+from starlette.responses import StreamingResponse
+import io
+
+app = FastAPI()
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+@app.post("/detect_faces/")
+async def detect_faces(file: UploadFile = File(...)):
+    image=await file.read()
+    gray_frame = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray_frame, scaleFactor=1.3, minNeighbors=5)
+    for (x, y, w, h) in faces:
+            # Draw a rectangle around the face
+        cv2.rectangle(image, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            # Save the face region as an image
+        face_roi = image[y:y+h, x:x+w]
+    img_bytes = face_roi.tobytes()
+    # Return the processed image
+    return StreamingResponse(io.BytesIO(img_bytes), media_type="image/png")
